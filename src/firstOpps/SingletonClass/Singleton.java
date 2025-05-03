25a952fd61ded7b6900e3f7b30e0198efc87fffc
Arunarivalagan743 <arunarivalagan774@gmail.com>
2025-05-03 08:34:20 +0530

    Singleton class is created

---
+++ src/firstOpps/SingletonClass/Singleton.java
@@ -0,0 +1,49 @@
+package firstOpps.SingletonClass;
+
+public class Singleton {  //It is to avoid multiple instance or objects .So it reamin same when the first object is create dof same values displayed on the every remaoing objects values be called
+
+    private static Singleton ins;
+
+
+    private String name;
+
+    public Singleton(String name) {
+        this.name = name;
+    }
+
+    private Singleton() {
+    }
+
+    public static Singleton instance() {
+        if (ins == null) { //when creae the first instance it will be and create a iinstance
+           Singleton ins = new Singleton();
+        }//otherwise it return last instance value
+        return ins;
+    }
+
+
+    public void setName(String name) {
+        if (this.name == null) {
+            this.name = name;
+        } else {
+            System.out.println("name is already exists");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Singleton obj = Singleton.instance();
+        obj.setName("Arun");
+
+        Singleton obj2 = Singleton.instance();
+        obj2.setName("Hari");
+
+        System.out.println(obj.getName());  // Arun
+        System.out.println(obj2.getName());  //Arun
+    }
+}
