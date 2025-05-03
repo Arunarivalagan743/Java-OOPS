1b97efd71a46b909e83e8c27cd6a5cbe14508e08
Arunarivalagan743 <arunarivalagan774@gmail.com>
2025-05-03 07:54:03 +0530

    Default and custom toString method to the object

--- src/firstOpps/StaticExample/InnerClass.java
+++ src/firstOpps/StaticExample/InnerClass.java
@@ -8,11 +8,16 @@ public class InnerClass {
         public Test(String name) {
             this.name = name;
         }
-    }
+
+       @Override
+       public String toString() {
+           return name;
+       }
+   }
     public static void main(String[] args) {
         Test t = new Test("arun");
         Test t1 = new Test("hari");
-
+        System.out.println(t);  //if we have custom to string to print name the return of string or it go to default tostring of has code
         System.out.println(t.name);
         System.out.println(t1.name);
 
